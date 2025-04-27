1c432b9df17e519ede1dcd76a32147815f3159c4
dmitriy.zadoya <zadoya.dmitriy@wb.ru>
2025-04-27 23:25:36 +0300

    homework 1

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -9,7 +10,26 @@ import (
 // go test -v homework_test.go
 
 func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+	var new uint32
+	for i := 0; i < 4; i++ {
+		new = new << 8
+		new |= uint32(byte(number >> (i * 8)))
+	} 
+	return new
+}
+
+type unsigned interface {
+	uint16 | uint32 | uint64
+}
+
+func ToLittleEndianGenerics[T unsigned](number T) T {
+	var new T
+	size := unsafe.Sizeof(number)
+	for i := 0; i < int(size); i++ {
+		new = new << 8
+		new |= T(byte(number >> (i * 8)))
+	} 
+	return new
 }
 
 func TestСonversion(t *testing.T) {
@@ -42,6 +62,111 @@ func TestСonversion(t *testing.T) {
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestToLittleEndianGenerics_Uint16(t *testing.T) {
+	tests := map[string]struct {
+		number uint16
+		result uint16
+	}{
+		"test case #1": {
+			number: 0x0000,
+			result: 0x0000,
+		},
+		"test case #2": {
+			number: 0xFFFF,
+			result: 0xFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF,
+			result: 0xFF00,
+		},
+		"test case #4": {
+			number: 0xAA55,
+			result: 0x55AA,
+		},
+		"test case #5": {
+			number: 0x0102,
+			result: 0x0201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndianGenerics(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestToLittleEndianGenerics_Uint32(t *testing.T) {
+	tests := map[string]struct {
+		number uint32
+		result uint32
+	}{
+		"test case #1": {
+			number: 0x00000000,
+			result: 0x00000000,
+		},
+		"test case #2": {
+			number: 0xFFFFFFFF,
+			result: 0xFFFFFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF00FF,
+			result: 0xFF00FF00,
+		},
+		"test case #4": {
+			number: 0x0000FFFF,
+			result: 0xFFFF0000,
+		},
+		"test case #5": {
+			number: 0x01020304,
+			result: 0x04030201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndianGenerics(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestToLittleEndianGenerics_Uint64(t *testing.T) {
+	tests := map[string]struct {
+		number uint64
+		result uint64
+	}{
+		"test case #1": {
+			number: 0x0000000000000000,
+			result: 0x0000000000000000,
+		},
+		"test case #2": {
+			number: 0xFFFFFFFFFFFFFFFF,
+			result: 0xFFFFFFFFFFFFFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF00FF00FF00FF,
+			result: 0xFF00FF00FF00FF00,
+		},
+		"test case #4": {
+			number: 0x00000000FFFFFFFF,
+			result: 0xFFFFFFFF00000000,
+		},
+		"test case #5": {
+			number: 0x0102030405060708,
+			result: 0x0807060504030201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndianGenerics(test.number)
 			assert.Equal(t, test.result, result)
 		})
 	}
